f904b0f54fffbc0e3fd94119db2567610b84e102
Sakae Watanabe <69649930+sfwatanabe@users.noreply.github.com>
2021-10-05 22:47:56 -0700

    Add caching for unpkg (#2)
    * Added localforage cache to dependencies.
    
    * Separate fetch plugin from path resolution.
    
    * Added .css support loading and esbuild from unpkg.

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,4 @@
 import * as esbuild from "esbuild-wasm";
-import axios from "axios";
 
 /**
  * Specifies actions to take when parsing url package information for requests
@@ -12,44 +11,24 @@ export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResolve", args);
-        if (args.path === "index.js") {
-          return { path: args.path, namespace: "a" };
-        }
+      // Path resolution for index.js request
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: "index.js", namespace: "a" };
+      });
 
-        if (args.path.includes("./") || args.path.includes("../")) {
-          return {
-            namespace: "a",
-            path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`)
-              .href,
-          };
-        }
+      // Path resolution for relative imports paths
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
         return {
           namespace: "a",
-          path: `https://unpkg.com/${args.path}`,
+          path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href,
         };
       });
 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log("onLoad", args);
-
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: `
-              import React, { useState } from 'react';
-              console.log(React, useState);
-            `,
-          };
-        }
-
-        const { data, request } = await axios.get(args.path);
-        // console.log(request);
+      // Default path resolution for top level packages
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
         return {
-          loader: "jsx",
-          contents: data,
-          resolveDir: new URL("./", request.responseURL).pathname,
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
         };
       });
     },
