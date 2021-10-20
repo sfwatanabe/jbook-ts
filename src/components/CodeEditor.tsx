a16bb84fb173deaceab7f6f79d79e78584ac1cf0
Sakae Watanabe <meltdownlv@gmail.com>
2021-10-19 21:31:55 -0700

    Add editor settings for monaco theme.

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,7 +1,29 @@
 import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor = () => {
-  return <MonacoEditor height="500px" language="javascript" />;
+  return (
+    <MonacoEditor
+      height="500px"
+      language="javascript"
+      theme="dark"
+      options={{
+        wordWrap: "on",
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 14,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+        rulers: [
+          {
+            column: 80,
+            color: "#DA4B81",
+          },
+        ],
+      }}
+    />
+  );
 };
 
 export default CodeEditor;
