2357b2871ade6be845cb9705b8616240ff176868
Sakae Watanabe <69649930+sfwatanabe@users.noreply.github.com>
2021-10-23 16:30:32 -0700

    Swatanabe/refactor remaining index (#6)
    * Move bundler and plugin logic to new dir.
    
    * Refactor code cell to stand alone component.
    
    * Refactor project structure.

--- src/index.tsx
+++ src/index.tsx
@@ -1,58 +1,13 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
-import * as esbuild from "esbuild-wasm";
-import { useState, useEffect, useRef } from "react";
+
 import ReactDOM from "react-dom";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-import { fetchPlugin } from "./plugins/FetchPlugin";
-import CodeEditor from "./components/CodeEditor";
-import Preview from "./components/Preview";
+import CodeCell from "./components/CodeCell/CodeCell";
 
-const App: React.FC = () => {
-  const ref = useRef<any>();
-  const [code, setCode] = useState("");
-  const [input, setInput] = useState("");
-
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
-  const onClick = async () => {
-    // Null guard for empty service ref
-    if (!ref.current) {
-      return;
-    }
-
-    const result = await ref.current.build({
-      entryPoints: ["index.js"],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        "process.env.NODE_ENV": '"production"',
-        global: "window",
-      },
-    });
-
-    setCode(result.outputFiles[0].text);
-  };
-
+const App = () => {
   return (
     <div>
-      <CodeEditor
-        initialValue={"const a = 1;"}
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 };
