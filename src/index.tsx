8929c2056d41159fa56d63e839f84e72c6742b81
Sakae Watanabe <meltdownlv@gmail.com>
2021-10-23 16:24:37 -0700

    Refactor code cell to stand alone component.

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,13 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 
-import { useState } from "react";
 import ReactDOM from "react-dom";
-
-import CodeEditor from "./components/CodeEditor";
-import Preview from "./components/Preview";
-import bundle from "./bundler";
+import CodeCell from "./components/CodeCell";
 
 const App = () => {
-  const [code, setCode] = useState("");
-  const [input, setInput] = useState("");
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue={"const a = 1;"}
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 };
