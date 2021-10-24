86ef9fdaa3a70900ff1a074de9a6eed954abeaf1
Sakae Watanabe <meltdownlv@gmail.com>
2021-10-23 17:09:39 -0700

    Add resizable library and basic css.

--- src/components/CodeCell/CodeCell.tsx
+++ src/components/CodeCell/CodeCell.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
+import bundle from "../../bundler";
 import CodeEditor from "./CodeEditor/CodeEditor";
 import Preview from "./Preview/Preview";
-import bundle from "../../bundler";
+import Resizable from "../Resizable/Resizable";
 
 const CodeCell = () => {
   const [code, setCode] = useState("");
@@ -14,16 +15,15 @@ const CodeCell = () => {
   };
 
   return (
-    <div>
-      <CodeEditor
-        initialValue={"const a = 1;"}
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
+    <Resizable direction={"vertical"}>
+      <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
+        <CodeEditor
+          initialValue={"const a = 1;"}
+          onChange={(value) => setInput(value)}
+        />
+        <Preview code={code} />
       </div>
-      <Preview code={code} />
-    </div>
+    </Resizable>
   );
 };
 
