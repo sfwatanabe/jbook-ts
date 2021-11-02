381d6878b0c3c13fa670f4aedfe5515bbba0fbd8
Sakae Watanabe <meltdownlv@gmail.com>
2021-11-01 21:54:49 -0700

    Update props and state for resizable dims.

--- src/components/CodeCell/CodeCell.tsx
+++ src/components/CodeCell/CodeCell.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import bundle from "../../bundler";
 import CodeEditor from "./CodeEditor/CodeEditor";
@@ -9,10 +9,16 @@ const CodeCell = () => {
   const [code, setCode] = useState("");
   const [input, setInput] = useState("");
 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 750);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
 
   return (
     <Resizable direction={"vertical"}>
