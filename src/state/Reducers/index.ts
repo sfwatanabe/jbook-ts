9e73742c01bc4eb7b59a7ecc2db7fa408a469332
Sakae Watanabe <69649930+sfwatanabe@users.noreply.github.com>
2022-07-16 18:25:41 -0700

    Setup redux state mgmt (#10)
    * Added redux dependencies and axios.
    
    * Setup redux directories and action enums.
    
    * Added enums and interfaces for cell actions
    
    * Added cells reducer interfaces.
    
    * Add redux store provider to index.tsx
    
    * Add redux actions and reducer logic

---
+++ src/state/Reducers/index.ts
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import cellsReducer from "./CellsReducer";
+
+const reducers = combineReducers({
+  cells: cellsReducer,
+});
+
+export default reducers;
+
+export type RootState = ReturnType<typeof reducers>;
